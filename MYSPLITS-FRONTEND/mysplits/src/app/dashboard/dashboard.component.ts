30d56a0494159b05a3a43177391742484851a261
saurabh devade <saurabh.devade21@gmail.com>
2020-02-26 18:13:01 +0000

    menus added in sidebar

--- MYSPLITS-FRONTEND/mysplits/src/app/dashboard/dashboard.component.ts
+++ MYSPLITS-FRONTEND/mysplits/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DASHBOARDComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public router:Router) { }
 
   ngOnInit() {
   }
 
+  navigateToGroups(){
+    this.router.navigate(['/dashboard/groups']);
+  }
 }
